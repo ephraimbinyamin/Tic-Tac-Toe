f4f5cddaf0262d0068f55b59580e55da67e5f81d
Ephraim Binyaminov <efraimbinyaminov16@gmail.com>
2022-11-18 04:12:40 +0200

    Initial commit Tic-Tac-Toe

---
+++ src/components/Symbol/Symbol.js
@@ -0,0 +1,25 @@
+import './symbol.scss';
+
+const getX = (    
+    `<div class="X">
+        <span></span>
+        <span></span>
+    </div>`
+);
+
+const getO = (    
+    `<div class="O">
+        <span></span>
+    </div>`
+);
+
+const setX = (cell) => {
+    cell.innerHTML = getX;
+}
+
+
+const setO = (cell) => {
+    cell.innerHTML = getO;
+}
+
+export { getX , setX , getO , setO};
